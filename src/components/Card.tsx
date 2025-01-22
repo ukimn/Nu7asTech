1a1cbc1e208b5de59eb1a137e4b8a5d56bdf2457
Moaath Ibrahim <no7asweb@gmail.com>
2025-01-22 18:35:29 +0300

    lis

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ const Card = ({imgUrl, name}: CardProps) => {
   const {showTitle, setShowTitle} = useShow();
   return (
     <div className="card" onPointerOver={()=>{setShowTitle(true)}} onPointerOut={()=>{setShowTitle(false)}}>
-      <img src={imgUrl} alt="card" height={"150px"} width={"150px"} loading="lazy"/>
+      <img src={imgUrl} alt="card" height={"150px"} width={"150px"}/>
       <h1 style={{visibility: showTitle ? "visible" : "hidden"}}>{name}</h1>
     </div>
   )
