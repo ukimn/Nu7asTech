dffbe60861089a34fca3c5bad383aeaae3c9947d
Moaath Ibrahim <no7asweb@gmail.com>
2025-01-23 21:03:48 +0300

    s93

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,18 +1,34 @@
-import {Data} from "../data";
+import { Data } from "../data";
 import Card from "./Card";
+import { useState } from "react";
 const Skills = () => {
+  const [search, setSearch] = useState<string>("");
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <>
-      <div className="main-gallery-container" id="skills">
+      <div
+        className="main-gallery-container"
+        id="skills"
+        style={{ display: "flex", flexDirection: "column" }}
+      >
         <div className="heading-container">
           <h1 className="heading">Skills</h1>
         </div>
+        <input type="text" value={search} onChange={handleInput} placeholder="search for a skill"/>
       </div>
       <div className="cards-gallery">
-        {Data.map((card) => {
+        {Data.filter((card) => {
+          return search.toLowerCase() === ""
+            ? card
+            : card.name.toLowerCase().includes(search);
+        }).map((card) => {
           return (
             <div key={card.id}>
-              <Card imgUrl={card.imgPath} name={card.name}/>
+              <Card imgUrl={card.imgPath} name={card.name} />
             </div>
           );
         })}
