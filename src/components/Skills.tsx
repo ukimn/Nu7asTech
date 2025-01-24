f45e9c3782c1fcb3fe23d1f3a741bd1e611a5127
Moaath Ibrahim <no7asweb@gmail.com>
2025-01-24 12:58:55 +0300

    enhance website Performance

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,16 +1,30 @@
 import { Data } from "../data";
 import Card from "./Card";
-import { useState } from "react";
+import { useReducer } from "react";
 import { FaFilter } from "react-icons/fa";
 
 const Skills = () => {
-  const [sort, setSort] = useState<number>(0);
+  const initValue = { count: 0 };
 
+  interface Action {
+    type: "Increment";
+  }
   const arrOfOptions = ["All", "Programming", "Design"];
 
-  const handleClick = () => {
-    setSort((prev) => (prev == arrOfOptions.length - 1 ? 0 : prev + 1));
-  };
+  function reducer(state: typeof initValue, action: Action) {
+    const { type } = action;
+    switch (type) {
+      case "Increment":
+        return {
+          ...state,
+          count: state.count == arrOfOptions.length - 1 ? 0 : state.count + 1,
+        };
+      default:
+        return state;
+    }
+  }
+
+  const [sort, dispatch] = useReducer(reducer, initValue);
 
   return (
     <>
@@ -22,16 +36,16 @@ const Skills = () => {
         <div className="heading-container">
           <h1 className="heading">Skills</h1>
         </div>
-        <button onClick={handleClick} className="sort-button">
-        {arrOfOptions[sort]} skills <FaFilter />
+        <button onClick={()=>{dispatch({type: "Increment"})}} className="sort-button">
+          {arrOfOptions[sort.count]} skills <FaFilter />
         </button>
       </div>
       <div className="cards-gallery">
         {Data.filter((card) => {
-          return arrOfOptions[sort] === "Programming"
+          return arrOfOptions[sort.count] === "Programming"
             ? card.type.includes("Programming")
             : card.type.includes("Design") ||
-                (arrOfOptions[sort] === "All" && card);
+                (arrOfOptions[sort.count] === "All" && card);
         }).map((card) => {
           return (
             <div key={card.id}>
