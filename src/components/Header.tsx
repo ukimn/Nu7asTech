d9d42073f0ad43cde0a7628c2e857389e30be89c
Moaath Ibrahim <no7asweb@gmail.com>
2025-01-27 20:33:41 +0300

    welcome

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,67 +2,69 @@ import { useTheme } from "./Context";
 import { useState } from "react";
 import { IoIosMoon } from "react-icons/io";
 import { IoSunnyOutline } from "react-icons/io5";
+import { motion } from "framer-motion";
 
 const Header = () => {
   const { theme, setTheme } = useTheme();
   const [isOpened, setIsOpened] = useState<boolean>(false);
   return (
-    <header id={`${theme ? "white" : "dark"}`}>
-      <nav className="navbar">
-        <h1 className="logo">n.w.b</h1>
-        <ul className={`words-navbar ${isOpened ? "open" : ""}`} id={`${theme ? "white" : "dark"}`}>
-          <li className="items">
-            <a
-              href="#home"
-              className="items"
-              id={`${theme ? "light-mode-color" : "dark-mode-color"}`}
-            >
-              Home
-            </a>
-          </li>
-          <li className="items">
-            <a
-              href="#skills"
-              className="items"
-              id={`${theme ? "light-mode-color" : "dark-mode-color"}`}
-            >
-              Info
-            </a>
-          </li>
-          <li className="items">
-            <a
-              href="#contact"
-              className="items"
-              id={`${theme ? "light-mode-color" : "dark-mode-color"}`}
-            >
-              Contact Us
-            </a>
-          </li>
-        </ul>
-        <div className="button-container">
-          <button
-            className="button-mode"
-            onClick={() => {
-              setTheme((prev) => !prev);
-            }}
-            id={`${theme ? "dark" : "white"}`}
+    <>
+      <header id={`${theme ? "white" : "dark"}`}>
+        <nav className="navbar">
+          <motion.h1 className="logo">n.w.b</motion.h1>
+          <ul
+            className={`words-navbar ${isOpened ? "open" : ""}`}
+            id={`${theme ? "white" : "dark"}`}
           >
-            {theme ? <IoIosMoon size={30}/> : <IoSunnyOutline size={30}/>}
-          </button>
-          <a className="button start" href="#contact">
-            Start
-          </a>
-        </div>
-        <div
-          className={`ham ${isOpened ? "open" : ""}`}
-          onClick={() => {
-            setIsOpened((prev) => !prev);
-          }}
-        >
-          <Hamburger isOpened={isOpened} />
-        </div>
-      </nav>
-    </header>
+            <li className="items">
+              <a
+                href="#home"
+                className="items"
+                id={`${theme ? "light-mode-color" : "dark-mode-color"}`}
+              >
+                Home
+              </a>
+            </li>
+            <li className="items">
+              <a
+                href="#skills"
+                className="items"
+                id={`${theme ? "light-mode-color" : "dark-mode-color"}`}
+              >
+                Info
+              </a>
+            </li>
+            <li className="items">
+              <a
+                href="#contact"
+                className="items"
+                id={`${theme ? "light-mode-color" : "dark-mode-color"}`}
+              >
+                Contact Us
+              </a>
+            </li>
+          </ul>
+          <div className="button-container">
+            <button
+              className="button-mode"
+              onClick={() => setTheme((prev) => !prev)}
+              id={`${theme ? "dark" : "white"}`}
+            >
+              {theme ? <IoIosMoon size={30} /> : <IoSunnyOutline size={30} />}
+            </button>
+            <a className="button start" href="#contact">
+              Start
+            </a>
+          </div>
+          <div
+            className={`ham ${isOpened ? "open" : ""}`}
+            onClick={() => setIsOpened((prev) => !prev)}
+          >
+            <Hamburger isOpened={isOpened} />
+          </div>
+        </nav>
+      </header>
+    </>
   );
 };
 
@@ -71,15 +73,13 @@ interface HamburgerProps {
 }
 
 function Hamburger({ isOpened }: HamburgerProps) {
-  const {theme} = useTheme();
+  const { theme } = useTheme();
   return (
-    <>
-      <div className={`hamburger ${isOpened ? "open" : ""}`}>
-        <div className="line" id={`${theme ? "dark" : "white"}`}></div>
-        <div className="line" id={`${theme ? "dark" : "white"}`}></div>
-        <div className="line" id={`${theme ? "dark" : "white"}`}></div>
-      </div>
-    </>
+    <div className={`hamburger ${isOpened ? "open" : ""}`}>
+      <div className="line" id={`${theme ? "dark" : "white"}`}></div>
+      <div className="line" id={`${theme ? "dark" : "white"}`}></div>
+      <div className="line" id={`${theme ? "dark" : "white"}`}></div>
+    </div>
   );
 }
 
--- src/elementError/Error.tsx
+++ src/elementError/Error.tsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom"
 import notFound from '../assets/notFound.json'
 import { Player } from "@lottiefiles/react-lottie-player"
+import { motion } from "framer-motion"
 const Error = () => {
   return (
-    <div className="error-page">
+    <motion.div className="error-page" initial={{x: '100vw'}} animate={{x: 0}} transition={{type: "spring", duration: 0.9, delay: 0.6}}>
         <h1 className="heading">PAGE NOT FOUND 404!</h1>
         <Player src={notFound} loop autoplay style={{width: "300px"}}/>
         <Link to={"/"}>
             <button className="button">Go Back</button>
         </Link>
-    </div>
+    </motion.div>
   )
 }
 
