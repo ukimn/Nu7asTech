51476da1e6293f49ebf59555e9347764ac5523f3
Moaath Ibrahim <no7asweb@gmail.com>
2025-01-30 22:25:26 +0300

    add some changes

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -1,25 +1,43 @@
-import img from "../assets/darkMode.webp";
-import lightMode from '../assets/lightMode.webp'
 import { useTheme } from "./Context";
+import { motion } from "framer-motion";
+import { Player } from "@lottiefiles/react-lottie-player";
+import coding from "../assets/coding.json";
 
 const MainBody = () => {
-  const {theme} = useTheme();
+  const { theme } = useTheme();
   return (
     <div className="main-body" id="home">
-      <div className="paragraph-container">
+      <motion.div
+        className="paragraph-container"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 2 }}
+        transition={{ duration: 1 }}
+      >
         <h3 className="paragraph" id={`${theme ? "white" : "dark"}`}>
-          <span className="span-logo">Nu<span className="num-sev">7</span>as Tech</span> is a leading company in
-          web design and development, specializing in delivering innovative and
-          customized digital solutions tailored to client needs.
+          <span className="span-logo">
+            Nu<span className="num-sev">7</span>as Tech
+          </span>{" "}
+          is a leading company in web design and development, specializing in
+          delivering innovative and customized digital solutions tailored to
+          client needs.
+          <h4
+            style={{ opacity: 0.8 }}
+            id={`${theme ? "white" : "dark"}`}
+            className="paragraph-sub"
+          >
+            Video Editors | Web Developers
+          </h4>
         </h3>
-      </div>
-      <div className="image-container">
-        <img
-          src={theme ? lightMode : img}
-          alt="a programming image"
-          height={"400px"}
-          width={"400px"}
-          className="img"
+      </motion.div>
+      <div
+        className="image-container"
+        style={{ display: "flex", justifyContent: "center"}}
+      >
+        <Player
+          autoplay
+          src={coding}
+          loop
+          style={{ height: "400px", width: "400px" }} // Increase the size here
         />
       </div>
     </div>
